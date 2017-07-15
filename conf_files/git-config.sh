1697ef4306783f4658b1508d6491230ed2c292d3
Michal Kusmierczyk <kusm@protonmail.ch>
2017-07-15 21:47:11 +0200

    Initial commit. Vim, tmux, bash, git and apps.

---
+++ conf_files/git-config.sh
@@ -0,0 +1,4 @@
+git config --global alias.s status
+git config --global alias.ch checkout
+git config --global alias.ci commit
+git config --global alias.l "log --oneline"
---
+++ set_all_up.sh
@@ -0,0 +1,7 @@
+cp conf_files/bashrc ~/.bashrc
+cp conf_files/vimrc ~/.vimrc
+cp conf_files/tmux.conf ~/.tmux.conf
+cp conf_files/git-prompt.sh ~/.git-prompt.sh
+cp conf_files/git-completion.bash ~/.git-completion.bash
+cp -r conf_files/gconf ~/.gconf
+sudo ./conf_files/install-apps-alternative.sh
