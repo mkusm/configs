301af4bb8c3fbfd2809552feeacb57d037fbb1e0
Michał Kuśmierczyk <mk@greensteam.com>
2017-07-19 18:47:50 +0200

    add bashrc comments; improve vimrc; improve main script

--- set_all_up.sh
+++ set_all_up.sh
@@ -1,8 +1,16 @@
-cp conf_files/bashrc ~/.bashrc
-cp conf_files/vimrc ~/.vimrc
-cp conf_files/tmux.conf ~/.tmux.conf
+# APPS
+sudo ./conf_files/install-apps-alternative.sh
+
+# BASH, TMUX
 cp conf_files/git-prompt.sh ~/.git-prompt.sh
 cp conf_files/git-completion.bash ~/.git-completion.bash
-cp -r conf_files/gconf ~/.gconf
-sudo ./conf_files/install-apps-alternative.sh
+cp conf_files/bashrc ~/.bashrc
+cp conf_files/tmux.conf ~/.tmux.conf
+
+# copy gnome-terminal settings to enable tmux by default, need fixing
+# cp -r conf_files/gconf ~/.gconf
+
+# VIM
+cp conf_files/vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir ~/.vim/backup
