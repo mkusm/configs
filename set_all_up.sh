a8d9b54f896215575f8920febcde242d529471f5
Michał Kuśmierczyk <mk@greensteam.com>
2017-07-19 18:00:02 +0200

    clear up vimrc; fix html and python indentation; add vundle installation

--- set_all_up.sh
+++ set_all_up.sh
@@ -5,3 +5,4 @@ cp conf_files/git-prompt.sh ~/.git-prompt.sh
 cp conf_files/git-completion.bash ~/.git-completion.bash
 cp -r conf_files/gconf ~/.gconf
 sudo ./conf_files/install-apps-alternative.sh
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
