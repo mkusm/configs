9aad9727f41e464e518f13919ff904487f33e3f8
Michal Kusmierczyk <kusm@protonmail.ch>
2017-08-14 15:45:12 +0200

    add keepassxc with config; switch tmux leader to right shift

--- set_all_up.sh
+++ set_all_up.sh
@@ -1,5 +1,5 @@
 # APPS
-sudo ./conf_files/install-apps-alternative.sh
+sudo ./conf_files/install-apps.sh
 
 # BASH, TMUX
 cp conf_files/git-prompt.sh ~/.git-prompt.sh
@@ -7,10 +7,8 @@ cp conf_files/git-completion.bash ~/.git-completion.bash
 cp conf_files/bashrc ~/.bashrc
 cp conf_files/tmux.conf ~/.tmux.conf
 
-# copy gnome-terminal settings to enable tmux by default, need fixing
-# cp -r conf_files/gconf ~/.gconf
-
 # VIM
 cp conf_files/vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir ~/.vim/backup
+echo "Remember to run :PluginInstall inside vim!"
